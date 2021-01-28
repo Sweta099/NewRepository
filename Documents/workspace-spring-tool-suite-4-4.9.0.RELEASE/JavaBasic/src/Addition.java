b6b2e9e3cb498c7f061ab2978880befc47e4872d
Sweta099 <swetapanda021@gmail.com>
2021-01-28 12:52:26 +0530

    first commit

---
+++ Documents/workspace-spring-tool-suite-4-4.9.0.RELEASE/JavaBasic/src/Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Addition {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first number :");
+		a=sc.nextInt();
+		System.out.println("Enter second number :");
+		b=sc.nextInt();
+		System.out.println("Addition is :" +(a+b));
+		
+
+	}
+
+}
